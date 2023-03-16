2a42fcfeacf87725411ab1f6d522184eec2f7af8
naranujo <naraujo@udesa.edu.ar>
2023-03-16 17:39:58 -0300

    Primer commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import openai
+import datetime as dt
+from twilio.rest import Client
+from dotenv import load_dotenv
+from flask import Flask, request, jsonify
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
+app = Flask(__name__)
+client = Client(account_sid, auth_token)
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({"status": 200, "message": "Hello World"})
+
+@app.route('/test', methods=['POST'])
+def test():
+    data = request.get_json()
+    return jsonify({"data": data, "status": 200, "Datetime": dt.datetime.now().strftime("%Y-%b-%d %H:%M:%S")})
+
+@app.route('/send', methods=['POST'])
+def send():
+    data = request.get_json()
+    # response = openai.Completion.create(
+    #     model="text-davinci-003",
+    #     prompt=data['q'],
+    #     temperature=0.7,
+    #     max_tokens=256,
+    #     top_p=1,
+    #     frequency_penalty=0,
+    #     presence_penalty=0
+    # )
+    # res_string = response.choices[0].text.strip()
+    # print(res_string)
+    msg = client.messages.create(
+        from_ = 'whatsapp:+14155238886',
+        to = "whatsapp:+5491135646079",
+        # body=res_string,
+        body = "Hola, soy un bot de prueba",
+    )
+    print("SID:",msg.sid)
+    return jsonify({"status": 200})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
